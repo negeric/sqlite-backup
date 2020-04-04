aa6cc3354e16cf63907aa47aad58da6b7ab3ccde
negeric <jeff@jeffanderson.me>
2020-04-04 10:29:10 -0400

    fixing date diff issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,18 +21,24 @@ fi
 
 ## Perform retention policy
 if [ -z "${DAYS_TO_KEEP}" ]; then
+    echo "No retention policy set.  Job is complete"
+else
+    echo "Retention policy configured.  Deleting backups older than ${DAYS_TO_KEEP} days"
     s3cmd --config=/s3cmd/s3cmd ls s3://${BUCKET} | while read -r line;
         do
-            createDate=`echo $line | awk {'print $1" "$2'}`
-            createDate=`date -d"$createDate" +%s`
-            olderThan=`date -d"-${DAYS_TO_KEEP}" +%s`
-            if [[ $createDate -lt $olderThan ]]
+            createDate=`echo $line | awk {'print $1'}`
+            currentDate=`date +'%Y-%m-%d'`
+            dateDiff=`( `date -d $B +%s` - `date -d $A +%s`) / (24*3600) | bc -l`
+            echo createDate
+            echo currentDate
+            echo dateDiff
+            if [[ $dateDiff -gt $olderThan ]]
             then 
                 fileName=`echo $line|awk {'print $4'}`
                 echo "$fileName is older than ${DAYS_TO_KEEP} days, deleting"
                 if [[ $fileName != "" ]]
                 then
-                    s3cmd --config=/s3cmd/s3cmd del "$fileName"
+                    #s3cmd --config=/s3cmd/s3cmd del "$fileName"
                 fi
             fi
         done;
